b87b4f6900bc2c05bcf4082127b44965c1678ab0
Carlos Soto <carlos.a.soto.gamez@gmail.com>
2024-11-13 22:06:39 -0600

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({ children }) {
 			<div id="main-header-loading">{fetching > 0 && <progress />}</div>
 			<header id="main-header">
 				<div id="header-title">
-					<h1>React Events</h1>
+					<h1>Local Events</h1>
 				</div>
 				<nav>{children}</nav>
 			</header>
